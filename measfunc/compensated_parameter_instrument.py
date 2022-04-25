ffec53aa262e9923f0ba42a8305b7da033c7303c
Rasmus B. Christensen <rasmus.christensen@nbi.ku.dk>
2022-04-25 18:16:46 +0200

    add multiple compesating channels

--- measfunc/compensated_parameter_instrument.py
+++ measfunc/compensated_parameter_instrument.py
@@ -1,31 +1,23 @@
+from qcodes import InstrumentChannel
 from qcodes.instrument.base import Instrument
 from qcodes.instrument.parameter import Parameter
 from qcodes.utils.validators import Numbers
+from typing import Union, Sequence
 
 
 class CompensatedParameterInstrument(Instrument):
     def __init__(self, name: str,
                  prime_parameter: Parameter,
-                 compensating_parameter: Parameter,
+                 compensating_parameter: Union[Parameter, Sequence[Parameter]],
                  *args, **kwargs):
         super().__init__(name=name, *args, **kwargs)
         self.prime_parameter = prime_parameter
-        self.compensating_parameter = compensating_parameter
-        self.add_parameter('slope',
-                           initial_value=0,
-                           unit='a.u.',
-                           label='Slope',
-                           vals=Numbers(-100, 100),
-                           get_cmd=None,
-                           set_cmd=None)
+        if type(compensating_parameter) == Parameter:
+            compensating_parameter = (compensating_parameter,)
 
-        self.add_parameter('intercept',
-                           initial_value=0,
-                           unit='V',
-                           label='Slope',
-                           vals=Numbers(-1, 1),
-                           get_cmd=None,
-                           set_cmd=None)
+        for i, para in enumerate(compensating_parameter):
+            ch = CompensatingChannel(self, f'ch{i+1}', para, 0, 0)
+            self.add_submodule(f'ch{i+1}', ch)
 
         self.add_parameter('compensatedparameter',
                            initial_value=self.prime_parameter.get(),
@@ -42,9 +34,38 @@ class CompensatedParameter(Parameter):
 
     def set_raw(self, value):
         self.root_instrument.prime_parameter(value)
-        slope = self.root_instrument.slope()
-        intercept = self.root_instrument.intercept()
-        self.root_instrument.compensating_parameter(value*slope+intercept)
+        channel_dict = self.root_instrument.submodules
+        for ch in channel_dict.keys():
+            comp_ch = channel_dict[ch]
+            slope = comp_ch.slope()
+            intercept = comp_ch.intercept()
+            comp_ch.compensating_parameter(value*slope+intercept)
 
     def get_raw(self):
         return self.root_instrument.prime_parameter.get()
+
+
+class CompensatingChannel(InstrumentChannel):
+    def __init__(self, parent: Instrument,
+                 name: str,
+                 compensating_parameter: Parameter,
+                 slope: float, intercept: float, **kwargs):
+        super().__init__(parent, name, **kwargs)
+
+        self.compensating_parameter = compensating_parameter
+
+        self.add_parameter('slope',
+                           initial_value=slope,
+                           unit='a.u.',
+                           label='Slope',
+                           vals=Numbers(-100, 100),
+                           get_cmd=None,
+                           set_cmd=None)
+
+        self.add_parameter('intercept',
+                           initial_value=intercept,
+                           unit='V',
+                           label='Intercept',
+                           vals=Numbers(-1, 1),
+                           get_cmd=None,
+                           set_cmd=None)
--- measfunc/tests/test_compensated_parameter.py
+++ measfunc/tests/test_compensated_parameter.py
@@ -1,9 +1,10 @@
-from qcodes.tests.instrument_mocks import DummyInstrument, DummyChannel
+from qcodes.tests.instrument_mocks import DummyInstrument
 from measfunc.compensated_parameter_instrument import CompensatedParameterInstrument
 
-def test_CompensatedParameterInstrument():
+
+def test_compensate_by_one_parameter():
     dac = DummyInstrument(name='dac')
-    compensated_parameter_instrument = CompensatedParameterInstrument(name= 'test_compensation',
+    compensated_parameter_instrument = CompensatedParameterInstrument(name='test_compensation',
                                                                       prime_parameter=dac.dac1,
                                                                       compensating_parameter=dac.dac2)
 
@@ -20,4 +21,28 @@ def test_CompensatedParameterInstrument():
 
     assert compensated_parameter_instrument.ch1.compensating_parameter() == 5
     assert compensated_parameter_instrument.ch1.compensating_parameter() == dac.dac2()
-test_CompensatedParameterInstrument()
+
+def test_compensate_by_many_parameters():
+    dac = DummyInstrument(name='dac')
+    compensated_parameter_instrument = CompensatedParameterInstrument(name='test_compensation',
+                                                                      prime_parameter=dac.dac1,
+                                                                      compensating_parameter=(dac.dac2, dac.dac3))
+
+    compensated_parameter_instrument.compensatedparameter(2)
+    assert compensated_parameter_instrument.compensatedparameter() == 2
+    assert compensated_parameter_instrument.compensatedparameter() == dac.dac1()
+
+    compensated_parameter_instrument.ch1.slope(1)
+    compensated_parameter_instrument.ch1.intercept(1)
+    compensated_parameter_instrument.ch2.slope(2)
+    compensated_parameter_instrument.ch2.intercept(2)
+    compensated_parameter_instrument.compensatedparameter(4)
+
+    assert compensated_parameter_instrument.compensatedparameter() == 4
+    assert compensated_parameter_instrument.compensatedparameter() == dac.dac1()
+
+    assert compensated_parameter_instrument.ch1.compensating_parameter() == 5
+    assert compensated_parameter_instrument.ch1.compensating_parameter() == dac.dac2()
+    assert compensated_parameter_instrument.ch2.compensating_parameter() == 10
+    assert compensated_parameter_instrument.ch1.compensating_parameter() == dac.dac2()
+
